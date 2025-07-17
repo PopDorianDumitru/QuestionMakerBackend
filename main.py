8aaa0959b418d5c5ba61ee7d8262401d19b15e38
dorianpop1512@gmail.com <uratenie50@gmail.com>
2025-07-17 17:28:06 +0300

    Fixed dependencies and added any origin

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ origins = settings.frontend_origins
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # whitelist your frontend origins explicitly
+    allow_origins=["*"],  # whitelist your frontend origins explicitly
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # only needed methods
     allow_headers=["Authorization", "Content-Type", "Accept"],  # restrict to needed headers
